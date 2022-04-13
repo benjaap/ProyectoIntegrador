12c814fc09150749bc66e121d87b0f531a6c3a5e
Benjamin <bpena@udesa.edu.ar>
2022-04-13 09:10:27 -0300

    DB

--- db/productos.js
+++ db/productos.js
@@ -4,5 +4,83 @@
 // La información de los productos debe mostrarse en todos los lugares de la maqueta en donde aparezca un listado de productos por ejemplo en home y en  “mi perfil”.
 
 datosProductos : [
-    
-]
+    {
+        id: 1,
+        marca: "Audi",
+        modelo: "TTRS",
+        color: "Nardo Gray",
+        peso: "1524kg",
+        potencia: "400cv",
+    }, {
+        id:"2",
+        marca:"BMW",
+        modelo: "m340i xDrive",
+        color: "Mineral Gray Metallic",
+        peso: "1810kg",
+        potencia: "387cv",
+    }, {
+
+    }, {
+        id: "",
+        marca: "",
+        modelo: "",
+        color: "",
+        peso: "",
+        potencia: "",
+    }]
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }, {
+
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }, {
+//         id:,
+//         marca:,
+//         modelo: ,
+//         color: ,
+//         peso: ,
+//         potencia: ,
+//     }
+
+// ]
+
+// module.exports=datosProductos;
