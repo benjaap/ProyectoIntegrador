f9ef84894e3e2f4406fa4399b0a88804d0455031
ManuelEspora <mespora@udesa.edu.ar>
2022-04-18 10:14:40 -0300

    Cambios hipervinculos

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,6 @@ const productos = require('../db/productos')
 
 const productController = {
     detalle: function(req, res){
-        let datosProductos = 'Audi TT';
         res.render('product', { producto: "datosProductos"})
     },
     Add: function(req, res){
