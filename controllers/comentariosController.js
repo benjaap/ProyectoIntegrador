df534131a764b91744deecc7e09a1d1398dd1e30
Benjamin <bpena@udesa.edu.ar>
2022-04-18 23:59:06 -0300

    comentarios index

--- controllers/comentariosController.js
+++ controllers/comentariosController.js
@@ -4,6 +4,9 @@ const comentarios = require('../db/comentarios');
 const comentariosController = {
     comment: function(req, res){
         return res.render('product', {comentario: data.comentario});
+    },
+    comment2: function(req,res){
+        return res.render('index', {comentario: data.comentario})
     }
 }
 
