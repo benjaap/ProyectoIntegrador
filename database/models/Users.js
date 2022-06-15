9613e992b6be433d4d9883f8e6c0a2e20626fe4a
martinlotzUD <mlotz@udesa.edu.ar>
2022-06-15 09:35:27 -0300

    coneccion de la base de datos a traves de sequelize creando el archivo js correspondiente y creacion de los modelos. Users completado.

---
+++ database/models/Users.js
@@ -0,0 +1,53 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = "users";
+    let cols = {
+    
+        id:  {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        name: {
+            type: dataTypes.STRING
+        },
+        surname: {
+            type: dataTypes.STRING
+        },
+        birthdate : {
+            type: dataTypes.DATE,
+            allowNull:true,
+        },
+        created_at : {
+            type: dataTypes.DATE,
+            allowNull:true,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        password: {
+            type: dataTypes.STRING
+        },
+        dni: {
+            type: dataTypes.STRING
+        },
+        avatar:{
+            type: dataTypes.STRING
+        }
+
+    }
+
+
+
+    }
+
+   let config = {
+    timestamps:true,
+   };
+
+   const Users = sequelize.define(alias, cols, config);
+
+   return Users;
