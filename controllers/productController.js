6de42b8710f691604c601b2c99976567fcd0412a
ManuelEspora <mespora@udesa.edu.ar>
2022-06-23 10:07:55 -0300

    merge

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,10 @@
 const db = require('./../database/models/index');
 const bycript = require('bcryptjs');
-const {Op, where} = require('sequelize');
+const {Op} = require('sequelize');
 
 const productController = {
-    show: function(req, res){ 
+    show: function(req, res){ //REVISAR
         let id = req.params.id
-        let auth = null 
-        if(req.session.auth){
-            auth = req.session.auth
-        } 
 
         db.products.findByPk(id, {
             include: [
@@ -16,63 +12,27 @@ const productController = {
                 {association: 'comments'}
             ]
         })
-
         .then((data)=>{
             res.render('product', {product: data})
         })
-
         .catch((err)=>{
             console.log(err)
         })
-        
-        const prod = db.Products.findAll() //Revisar
-        .then(function(prod){const auth2 = {name: prod.name, description: prod.description, image: prod.url_image, }
-        }
-        )
-
-        db.users.create(productDetail)
-            .then(data => {
-                res.redirect('/')
-            })
-            .catch(e=>{
-                console.log(e)
-                res.send("Error al crear el usuario")
-            })
-        
-        res.render('product', {Title: 'Detalle Producto', auth})
     },
-    search: async function(req,res){
+    list: async function(req,res){
         const search = req.query.search
         let productos = []
         if(search){
             const consulta = {name: {[Op.like]: '%'+search+'%'}}
-            productos = await db.products.findAll()
+            productos = await db.products.findAll({
+                where: consulta
+            })
         } else{
             productos = await db.products.findAll()
         }
-        
-
-        let auth = null 
-        if(req.session.auth){
-            auth = req.session.auth
-            res.render('productsList', {productos}, auth)
-        } else{
-            res.redirect('/')
-        }
-        
+        res.render('productList', {productos})
     },
-    add: function(req,res){
-        let auth = null 
-        if(req.session.auth){
-            auth = req.session.auth
-        } 
-        res.render('Add', {Title:'Productos', auth})
-    },
-<<<<<<< HEAD
-    store: function(req, res){
-=======
     store: function(req, res){ //Revisar
->>>>>>> 13efd8aacb5f24b9c399896e423b3dee912baf03
         let data = req.body;
         let newProduct = {
             name: req.body.name,
